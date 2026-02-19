0a64c32d4870706e52d49630ad96afb514d22b58
TEswarreddy <thathieswarreddy@gmail.com>
2026-02-19 22:04:08 +0530

    Revert "feat: Implement authentication and product management features"
    This reverts commit b393048b45d8a0c9f8c0fde3344422aa6c1558fd.

--- frontend/src/app/services/order.ts
+++ frontend/src/app/services/order.ts
@@ -1,29 +1,8 @@
-import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { environment } from '../../environments/environment';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
-export class OrderService {
-
-  private apiUrl = environment.apiUrl;
-
-  constructor(private http: HttpClient) {}
-
-  placeOrder(data: any) {
-    return this.http.post(`${this.apiUrl}/orders`, data);
-  }
-
-  getMyOrders() {
-    return this.http.get(`${this.apiUrl}/orders/myorders`);
-  }
-
-  getAllOrders() {
-    return this.http.get(`${this.apiUrl}/orders`);
-  }
-
-  updateOrder(id: string, data: any) {
-    return this.http.put(`${this.apiUrl}/orders/${id}`, data);
-  }
+export class Order {
+  
 }
